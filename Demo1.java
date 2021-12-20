3d590a76660177881c00f677c4b928fc24483fce
Venkatalucky <abburivenkatakalyan1@gmail.com>
2021-12-20 18:03:12 +0530

    changed in java

---
+++ Demo1.java
@@ -0,0 +1,14 @@
+public class Demo1
+{
+    int a =2;
+    int b =3;
+    public static void main(String [] args)
+    {
+        Demo demo = new Demo();
+        System.out.println("main starts");
+        System.out.println(demo.a);
+        System.out.println(demo.b);
+        System.out.println("Main ends");
+        
+    }
+}
